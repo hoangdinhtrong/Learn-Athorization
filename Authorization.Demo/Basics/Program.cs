2613a1101998c4f29ad9563afb60fd51cb2210ab
hoangdinhtrong <info.hoangdinhtrong.hy@gmail.com>
2023-06-04 22:52:20 +0700

    Authenticate - Basics - Add Authorization

--- Authorization.Demo/Basics/Program.cs
+++ Authorization.Demo/Basics/Program.cs
@@ -1,3 +1,7 @@
+using Basics.AuthorizationRequirements;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,12 +11,38 @@ builder.Services.AddAuthentication("CookieAuth").AddCookie("CookieAuth", configu
     configureOptions.LoginPath = "/Home/Authenticate";
 });
 
+builder.Services.AddAuthorization(config =>
+{
+    //var defaultAuthBuilder = new AuthorizationPolicyBuilder();
+    //var defaultPolicy = defaultAuthBuilder.RequireAuthenticatedUser()
+    //    .RequireClaim(ClaimTypes.DateOfBirth)
+    //    .Build();
+    //config.DefaultPolicy = defaultPolicy;
+
+    //config.AddPolicy("Claim.DoB", policyBuilder =>
+    //{
+    //    policyBuilder.RequireClaim(ClaimTypes.DateOfBirth);
+    //});
+
+    config.AddPolicy("Admin", policyBuilder =>
+    {
+        policyBuilder.RequireClaim(ClaimTypes.Role, "Admin");
+    });
+
+    config.AddPolicy("Claim.DoB", policyBuilder =>
+    {
+        policyBuilder.RequireCustomClaim(ClaimTypes.DateOfBirth);
+    });
+});
+
+builder.Services.AddScoped<IAuthorizationHandler, CustomRequireClaimHandler>();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
